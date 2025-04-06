9e143b9d3d94f224b801709153a642646b4b4d00
Thejashree <theju11304@gmail.com>
2025-04-06 14:46:19 +0530

    Encapsulation class work

---
+++ boat-system/src/com/xworkz/boatapp/boat/Boat.java
@@ -0,0 +1,54 @@
+package com.xworkz.boatapp.boat;
+
+public class Boat
+{
+    private int boatId;
+    private String boatName;
+    private String boatOwner;
+    private String boatType;
+
+
+    public void setBoatId(int id)
+    {
+        boatId=id;
+    }
+
+    public int getBoatId()
+    {
+        return boatId;
+    }
+
+    public void setBoatName(String name)
+    {
+        boatName=name;
+    }
+
+    public String getBoatName()
+    {
+        return boatName;
+    }
+
+    public void setBoatOwner(String OwnerName)
+    {
+        boatOwner=OwnerName;
+    }
+
+    public String getBoatOwner()
+    {
+        return boatOwner;
+    }
+
+    public void setBoatType(String boatType)
+    {
+        this.boatType=boatType;
+    }
+    public String getBoatType()
+    {
+        return boatType;
+    }
+}
+//public class
+//private datamembers
+//default constructor
+//public getters(accessors) and public setters(mutators).
+//not knowing.................................................
